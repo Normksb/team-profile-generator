4e48142bc5e37ae41e147b7e655d6f25f7fd3195
Norman Bernard <nksb414@gmail.com>
2021-05-24 08:20:36 +0000

    added add manager to team function and logged html output

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-const intern = require('./lib/intern.js');
-const manager = require('./lib/manager.js');
-const employee = require('./lib/employee.js');
-const engineer = require('./lib/engineer.js');
-const manager = require('../../inquirer/manager.js');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const intern = require("./lib/intern.js");
+const manager = require("./lib/manager.js");
+const employee = require("./lib/employee.js");
+const engineer = require("./lib/engineer.js");
+let currentTeam = ``;
 
-inquirer.prompt([
+inquirer
+  .prompt([
     {
       type: "input",
       message: "Please enter your team managers name",
@@ -35,20 +36,68 @@ inquirer.prompt([
       choices: ["Engineer", "Intern", "Finish"],
     },
   ])
-  
+
   .then((answers) => {
     console.log(answers);
-    let manager = new manager(
-        answers.managerName,
-        answers.managerID,
-        answers.managerEmail,
-        answers.managerPhone
+    let teamManager = new manager(
+      answers.managerName,
+      answers.managerID,
+      answers.managerEmail,
+      answers.managerPhone
     );
-    console.log(manager);
+    let managerHtml = addManagerToTeam(teamManager);
+    currentTeam = currentTeam + managerHtml;
+    if(answers.checkListOption == "Engineer"){
+        engineerQuestions();
+
+    } else if(answers.checkListOption == "Intern"){
+        internQuestions();
+    } else {
+        writeToFile();
+    }
+    // switch(answers.checkListOption){
+    //     case "Engineer":
+    //         engineerQuestions();
+    //         break;
+    //     case "Intern":
+    //         internQuestions();
+    //         break;
+    //     case "Finish":
+    //         writeToFile();
+    //         break;
+    //     default:
+    //         writeToFile();
+    //}
   });
 
-  function html(data){
-    return `<!DOCTYPE html>
+function engineerQuestions(){
+    console.log('engineer');
+}
+
+function internQuestions(){
+    console.log('intern');
+}
+
+function writeToFile(){
+    console.log('current team is... ' + currentTeam);
+}
+
+function addManagerToTeam(data){
+    return `
+    
+    <div>
+    <p>Name: ${data.name}</p>
+    <p>ID: ${data.id}</p>
+    <p>Email: ${data.email}</p>
+    <p>Office Number: ${data.officeNumber}</p>
+    </div>
+
+    `
+}
+
+
+function html(currentTeam) {
+  return `<!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
@@ -57,7 +106,7 @@ inquirer.prompt([
         <title>Document</title>
     </head>
     <body>
-        ${data}
+        ${currentTeam}
     </body>
-    </html>`
-    }
+    </html>`;
+}
